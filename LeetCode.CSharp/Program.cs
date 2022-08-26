32c937d7b0db15049c71848a2592ed4815bc63d1
Mohammad Toosi <m.h.t.1377@gmail.com>
2022-08-26 17:45:34 +0200

    add python project

--- LeetCode.CSharp/Program.cs
+++ LeetCode.CSharp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using LeetCode.Easy;
 
-Console.WriteLine("Hello, LeetCode!");
+Console.WriteLine("Hello, LeetCode.CSharp!");
 // Console.WriteLine(new E383().CanConstruct("aab", "baa"));
 // Console.WriteLine(new E9().IsPalindrome(-121));
 Console.WriteLine(new E14().LongestCommonPrefix(new []{"car", "apple"}));
---
+++ Leetcode.Python/main.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello leetcode!")
+
+
+if __name__ == '__main__':
+    main()
+
