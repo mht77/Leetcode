a82bf4d3afb2d840bbd6f126459d2707bd01eb1c
Mohammad Toosi <m.h.t.1377@gmail.com>
2022-09-25 18:45:29 +0200

    [CSharp] add hard 72

---
+++ LeetCode.CSharp/Hard/H72.cs
@@ -0,0 +1,25 @@
+namespace LeetCode.Hard;
+
+public class H72
+{
+    public int MinDistance(string word1, string word2)
+    {
+        var dis = new int[word1.Length+1, word2.Length+1];
+        for (int i = 0; i <= word1.Length; i++)
+        {
+            for (int j = 0; j <= word2.Length; j++)
+            {
+                if (i == 0)
+                    dis[i, j] = j;
+                else if (j == 0)
+                    dis[i, j] = i;
+                else if (word1[i-1] == word2[j-1])
+                    dis[i, j] = dis[i - 1, j - 1];
+                else
+                    dis[i, j] = 1 + Math.Min(dis[i, j - 1], Math.Min(dis[i - 1, j], dis[i - 1, j - 1]));
+            }
+        }
+        return dis[word1.Length, word2.Length];
+    }
+}
+
