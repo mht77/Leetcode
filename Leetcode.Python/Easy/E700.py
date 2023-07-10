ee992ca24d6617b41e3f48228314df680140dbf0
Mohammad Toosi <m.h.t.1377@gmail.com>
2023-07-11 01:45:49 +0200

    [Python] add easy 700

---
+++ Leetcode.Python/Easy/E700.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        return self.DFS(root, val)
+
+
+    def DFS(self, root: TreeNode, val):
+        if not root:
+            return
+
+        if val == root.val:
+            return root
+
+        r = self.DFS(root.right, val)
+
+        if r:
+            return r
+
+        r = self.DFS(root.left, val)
+
+        if r:
+            return r
+
+        return None
